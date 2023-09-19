ca81372a7c762e31574a96ec8e94d6dd00def55e
RazielFake <39502475+RazielFake@users.noreply.github.com>
2023-09-19 12:50:21 -0600

    Create someAndEveryMethod.js

---
+++ someAndEveryMethod.js
@@ -0,0 +1,11 @@
+function checkPositive(arr) {
+  return arr.every(function(value){
+    return value > 0
+  })
+}
+
+function checkPositive(arr) {
+  return arr.some(function(value){
+    return value>0
+  })
+}
